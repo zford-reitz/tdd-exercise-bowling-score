d3d7a9f862009c8157ed50ec88a344df2cb9f0fb
Zeb Ford-Reitz <zeb.fordreitz@gmail.com>
2020-02-20 18:06:26 +0100

    We know that an open final frame is complete

---
+++ src/main/java/de/bredex/qualitydojo/bowlingscore/TenthTenPinBowlingFrame.java
@@ -0,0 +1,14 @@
+package de.bredex.qualitydojo.bowlingscore;
+
+public class TenthTenPinBowlingFrame implements Frame {
+
+	@Override
+	public boolean isComplete() {
+		return true;
+	}
+
+	@Override
+	public void roll(int numberOfPinsKnockedDown) {
+	}
+
+}
